5c9e30012c8cb9b39e0386a9cf07fb42ea1165bc
Max <max.bergmark@gmail.com>
2019-01-25 08:09:13 +0100

    Added main and tasks

---
+++ main.py
@@ -0,0 +1,17 @@
+from redis import redis
+from rq import Queue
+import tasks
+import time
+
+c = Redis(host = "elissa-0")
+q = Queue(connection = c)
+
+t0 = time.time()
+jobs = []
+for i in range(100):
+	jobs.append(q.enqueue(tasks.newkeys, 1024))
+while any(not job.is_finished for job in jobs):
+	time.sleep(0.1)
+t1 = time.time()
+
+print(t1-t0)
