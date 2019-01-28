3d441c0d335aaff26c2b5045adec7cb843eb5976
Max <max.bergmark@gmail.com>
2019-01-28 08:38:57 +0000

    Added redis file

---
+++ forty-game/fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import *
+
+env.hosts = [
+	'elissa-0', 
+	'elissa-1'
+]
+env.key_filename = '~/.ssh/id_ecdsa'
+
+@parallel
+def update():
+	sudo('apt update')
+
+@parallel
+def ping():
+	run('echo "$(hostname) is up!"')
+
+@parallel
+def pull():
+	run('cd Documents/nanopi-cloud; git pull')
+
+@parallel
+def start():
+	run('cd Documents/nanopi-cloud; python3 worker.py')
